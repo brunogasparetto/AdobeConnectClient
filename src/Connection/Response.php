91e26724eb0f4c73eaf8d666d65b61acf8b0078c
Bruno Gasparetto <bruno@ead.ucdb.br>
2017-03-07 14:43:12 -0400

    Create ResponseInterface

--- src/Connection/Response.php
+++ src/Connection/Response.php
@@ -1,34 +1,49 @@
 <?php
 namespace Bruno\AdobeConnectClient\Connection;
 
-class Response
+/**
+ * The server response
+ */
+class Response implements ResponseInterface
 {
     /**
-     * @var array An array as Header => Value
+     * @var int
+     */
+    protected $statusCode = 0;
+
+    /**
+     * @var array An array as name => value. Value is an array
      */
     protected $headers = [];
 
     /**
-     * @var string The response body
+     * @var \Bruno\AdobeConnectClient\Connection\StreamInterface The response body
      */
-    protected $body = '';
+    protected $body = null;
 
     /**
      * Create the Response
      *
-     * @param array $headers An array as Header => Value
-     * @param string $body The response body
+     * @param int $statusCode The response status code
+     * @param array $headers An array as name => value. Value is an array of strings.
+     * @param \Bruno\AdobeConnectClient\Connection\StreamInterface $body The response body
      */
-    public function __construct(array $headers, $body)
+    public function __construct($statusCode, array $headers, StreamInterface $body)
     {
+        $this->statusCode = intval($statusCode);
         $this->headers = $headers;
         $this->body = $body;
+    }
+
+    public function getStatusCode()
+    {
+        return $this->statusCode;
     }
 
     /**
      * Get the Body
      *
-     * @return string
+     * @return \Bruno\AdobeConnectClient\Connection\StreamInterface
      */
     public function getBody()
     {
@@ -46,23 +61,85 @@ class Response
     }
 
     /**
-     * Get a Specific Header
+     * Get a specific header
      *
-     * @return string
+     * @param string $name The header name
+     * @return array
      */
-    public function getHeader($header)
+    public function getHeader($name)
     {
-        if (isset($this->headers[$header])) {
-            return $this->headers[$header];
+        if (isset($this->headers[$name])) {
+            return $this->headers[$name];
         }
-        $header = $this->normalizeString($header);
+        $name = $this->normalizeString($name);
 
-        foreach ($this->headers as $key => $value) {
-            if ($key === $header) {
+        foreach ($this->headers as $header => $value) {
+            if ($header === $name) {
                 return $value;
             }
         }
-        return '';
+        return [];
+    }
+
+    /**
+     * Get a specific header concat the values with comma
+     *
+     * @param string The header name
+     * @return string
+     */
+    public function getHeaderLine($name)
+    {
+        return implode(',', $this->getHeader($name));
+    }
+
+    /**
+     * Get the reason phrase
+     *
+     * @return string
+     */
+    public function getReasonPhrase()
+    {
+        switch ($this->statusCode) {
+            case 100: $text = 'Continue'; break;
+            case 101: $text = 'Switching Protocols'; break;
+            case 200: $text = 'OK'; break;
+            case 201: $text = 'Created'; break;
+            case 202: $text = 'Accepted'; break;
+            case 203: $text = 'Non-Authoritative Information'; break;
+            case 204: $text = 'No Content'; break;
+            case 205: $text = 'Reset Content'; break;
+            case 206: $text = 'Partial Content'; break;
+            case 300: $text = 'Multiple Choices'; break;
+            case 301: $text = 'Moved Permanently'; break;
+            case 302: $text = 'Moved Temporarily'; break;
+            case 303: $text = 'See Other'; break;
+            case 304: $text = 'Not Modified'; break;
+            case 305: $text = 'Use Proxy'; break;
+            case 400: $text = 'Bad Request'; break;
+            case 401: $text = 'Unauthorized'; break;
+            case 402: $text = 'Payment Required'; break;
+            case 403: $text = 'Forbidden'; break;
+            case 404: $text = 'Not Found'; break;
+            case 405: $text = 'Method Not Allowed'; break;
+            case 406: $text = 'Not Acceptable'; break;
+            case 407: $text = 'Proxy Authentication Required'; break;
+            case 408: $text = 'Request Time-out'; break;
+            case 409: $text = 'Conflict'; break;
+            case 410: $text = 'Gone'; break;
+            case 411: $text = 'Length Required'; break;
+            case 412: $text = 'Precondition Failed'; break;
+            case 413: $text = 'Request Entity Too Large'; break;
+            case 414: $text = 'Request-URI Too Large'; break;
+            case 415: $text = 'Unsupported Media Type'; break;
+            case 500: $text = 'Internal Server Error'; break;
+            case 501: $text = 'Not Implemented'; break;
+            case 502: $text = 'Bad Gateway'; break;
+            case 503: $text = 'Service Unavailable'; break;
+            case 504: $text = 'Gateway Time-out'; break;
+            case 505: $text = 'HTTP Version not supported'; break;
+            default: $text = ''; break;
+        }
+        return $text;
     }
 
     /**
