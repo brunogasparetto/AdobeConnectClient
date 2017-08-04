c8f53410e7f7c881ad4c96018681380774df7aa1
Bruno Gasparetto <bruno@ead.ucdb.br>
2017-08-04 15:31:29 -0400

    Create Parameter class to use in exceptional cases

---
+++ src/Parameter.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace AdobeConnectClient;
+
+use DateTimeInterface;
+use DateTime;
+use AdobeConnectClient\Helpers\BooleanTransform as BT;
+use AdobeConnectClient\Helpers\StringCaseTransform as SCT;
+use AdobeConnectClient\Helpers\ValueTransform as VT;
+
+/**
+ * A generic Parameter class to extra parameters.
+ */
+class Parameter implements ParameterInterface
+{
+    /** @var array */
+    protected $parameters = [];
+
+    public static function instance()
+    {
+        return new static;
+    }
+
+    /**
+     * Add a parameter
+     *
+     * @param string $parameter
+     * @param mixed $value
+     * @return Parameter Fluent Interface
+     */
+    public function set($parameter, $value)
+    {
+        $this->parameters[SCT::toHyphen($parameter)] = VT::toString($value);
+        return $this;
+    }
+
+    /**
+     * Remove a parameter
+     *
+     * @param string $parameter
+     * @return Parameter Fluent Interface
+     */
+    public function remove($parameter)
+    {
+        $parameter = SCT::toHyphen($parameter);
+
+        if (isset($this->parameters[$parameter])) {
+            unset($this->parameters[SCT::toHyphen($parameter)]);
+        }
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function toArray()
+    {
+        return $this->parameters;
+    }
+}
