a35df6594d0855c46c133c24f7674033f5bdb27f
Bruno Gasparetto <brunogasparetto@gmail.com>
2018-02-24 11:04:57 -0400

    Begin with unit tests

---
+++ tests/ParameterTest.php
@@ -0,0 +1,42 @@
+<?php
+
+use \PHPUnit\Framework\TestCase;
+use \AdobeConnectClient\Parameter;
+
+class ParameterTest extends TestCase
+{
+    public function testInstance()
+    {
+        $this->assertInstanceOf(Parameter::class, Parameter::instance());
+    }
+
+    public function testSet()
+    {
+        $parameter = Parameter::instance();
+
+        $parameter->set('scoId', 1);
+        $expected = ['sco-id' => 1];
+        $this->assertEquals($expected, $parameter->toArray());
+
+        $parameter->set('name', 'test');
+        $expected['name'] = 'test';
+        $this->assertEquals($expected, $parameter->toArray());
+
+        return $parameter;
+    }
+
+    /**
+     * @depends testSet
+     * @param Parameter $parameter
+     */
+    public function testRemove(Parameter $parameter)
+    {
+        $parameter->remove('scoId');
+        $expected = ['name' => 'test'];
+        $this->assertEquals($expected, $parameter->toArray());
+
+        $parameter->remove('name');
+        $expected = [];
+        $this->assertEquals($expected, $parameter->toArray());
+    }
+}
