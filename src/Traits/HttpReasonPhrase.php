2197809b90868e95a4ef98203ab024fdc322eb43
Bruno Gasparetto <brunogasparetto@gmail.com>
2018-02-25 19:47:24 -0400

    Change the big switch to array

--- src/Traits/HttpReasonPhrase.php
+++ src/Traits/HttpReasonPhrase.php
@@ -7,6 +7,67 @@ namespace AdobeConnectClient\Traits;
  */
 trait HttpReasonPhrase
 {
+    private static $phrases = [
+        100 => 'Continue',
+        101 => 'Switching Protocols',
+        102 => 'Processing',
+        200 => 'OK',
+        201 => 'Created',
+        202 => 'Accepted',
+        203 => 'Non-Authoritative Information',
+        204 => 'No Content',
+        205 => 'Reset Content',
+        206 => 'Partial Content',
+        207 => 'Multi-status',
+        208 => 'Already Reported',
+        300 => 'Multiple Choices',
+        301 => 'Moved Permanently',
+        302 => 'Found',
+        303 => 'See Other',
+        304 => 'Not Modified',
+        305 => 'Use Proxy',
+        306 => 'Switch Proxy',
+        307 => 'Temporary Redirect',
+        400 => 'Bad Request',
+        401 => 'Unauthorized',
+        402 => 'Payment Required',
+        403 => 'Forbidden',
+        404 => 'Not Found',
+        405 => 'Method Not Allowed',
+        406 => 'Not Acceptable',
+        407 => 'Proxy Authentication Required',
+        408 => 'Request Time-out',
+        409 => 'Conflict',
+        410 => 'Gone',
+        411 => 'Length Required',
+        412 => 'Precondition Failed',
+        413 => 'Request Entity Too Large',
+        414 => 'Request-URI Too Large',
+        415 => 'Unsupported Media Type',
+        416 => 'Requested range not satisfiable',
+        417 => 'Expectation Failed',
+        418 => 'I\'m a teapot',
+        422 => 'Unprocessable Entity',
+        423 => 'Locked',
+        424 => 'Failed Dependency',
+        425 => 'Unordered Collection',
+        426 => 'Upgrade Required',
+        428 => 'Precondition Required',
+        429 => 'Too Many Requests',
+        431 => 'Request Header Fields Too Large',
+        451 => 'Unavailable For Legal Reasons',
+        500 => 'Internal Server Error',
+        501 => 'Not Implemented',
+        502 => 'Bad Gateway',
+        503 => 'Service Unavailable',
+        504 => 'Gateway Time-out',
+        505 => 'HTTP Version not supported',
+        506 => 'Variant Also Negotiates',
+        507 => 'Insufficient Storage',
+        508 => 'Loop Detected',
+        511 => 'Network Authentication Required',
+    ];
+
     /**
      * Gets the response reason phrase associated with the status code.
      *
@@ -14,159 +75,8 @@ trait HttpReasonPhrase
      */
     public function getReasonPhrase()
     {
-        switch ($this->statusCode) {
-            case 100:
-                $text = 'Continue';
-                break;
-
-            case 101:
-                $text = 'Switching Protocols';
-                break;
-
-            case 200:
-                $text = 'OK';
-                break;
-
-            case 201:
-                $text = 'Created';
-                break;
-
-            case 202:
-                $text = 'Accepted';
-                break;
-
-            case 203:
-                $text = 'Non-Authoritative Information';
-                break;
-
-            case 204:
-                $text = 'No Content';
-                break;
-
-            case 205:
-                $text = 'Reset Content';
-                break;
-
-            case 206:
-                $text = 'Partial Content';
-                break;
-
-            case 300:
-                $text = 'Multiple Choices';
-                break;
-
-            case 301:
-                $text = 'Moved Permanently';
-                break;
-
-            case 302:
-                $text = 'Moved Temporarily';
-                break;
-
-            case 303:
-                $text = 'See Other';
-                break;
-
-            case 304:
-                $text = 'Not Modified';
-                break;
-
-            case 305:
-                $text = 'Use Proxy';
-                break;
-
-            case 400:
-                $text = 'Bad Request';
-                break;
-
-            case 401:
-                $text = 'Unauthorized';
-                break;
-
-            case 402:
-                $text = 'Payment Required';
-                break;
-
-            case 403:
-                $text = 'Forbidden';
-                break;
-
-            case 404:
-                $text = 'Not Found';
-                break;
-
-            case 405:
-                $text = 'Method Not Allowed';
-                break;
-
-            case 406:
-                $text = 'Not Acceptable';
-                break;
-
-            case 407:
-                $text = 'Proxy Authentication Required';
-                break;
-
-            case 408:
-                $text = 'Request Time-out';
-                break;
-
-            case 409:
-                $text = 'Conflict';
-                break;
-
-            case 410:
-                $text = 'Gone';
-                break;
-
-            case 411:
-                $text = 'Length Required';
-                break;
-
-            case 412:
-                $text = 'Precondition Failed';
-                break;
-
-            case 413:
-                $text = 'Request Entity Too Large';
-                break;
-
-            case 414:
-                $text = 'Request-URI Too Large';
-                break;
-
-            case 415:
-                $text = 'Unsupported Media Type';
-                break;
-
-            case 500:
-                $text = 'Internal Server Error';
-                break;
-
-            case 501:
-                $text = 'Not Implemented';
-                break;
-
-            case 502:
-                $text = 'Bad Gateway';
-                break;
-
-            case 503:
-                $text = 'Service Unavailable';
-                break;
-
-            case 504:
-                $text = 'Gateway Time-out';
-                break;
-
-            case 505:
-                $text = 'HTTP Version not supported';
-                break;
-
-            default:
-                $text = '';
-                break;
-        }
-        return $text;
+        return isset(static::$phrases[$this->statusCode])
+            ? static::$phrases[$this->statusCode]
+            : '';
     }
 }
