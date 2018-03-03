b85d479f3d9a8647feb08e4891039854a51bd8eb
Bruno Gasparetto <brunogasparetto@gmail.com>
2018-03-03 12:37:29 -0400

    Create a base test for commands

---
+++ tests/Commands/TestCommandBase.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bruno
+ * Date: 03/03/2018
+ * Time: 12:32
+ */
+
+namespace AdobeConnectClient\Tests\Commands;
+
+use AdobeConnectClient\Tests\Connection\File\Connection;
+use AdobeConnectClient\Client;
+use PHPUnit\Framework\TestCase;
+
+class TestCommandBase extends TestCase
+{
+    /**
+     * @var Connection
+     */
+    protected $connection;
+
+    /**
+     * @var Client
+     */
+    protected $client;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->connection = new Connection();
+        $this->client = new Client($this->connection);
+    }
+}
