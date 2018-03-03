1eef1fe5653f324f0cb5a1e2677e03f168c4dc4b
Bruno Gasparetto <brunogasparetto@gmail.com>
2018-03-03 12:37:51 -0400

    Create tests for AclFieldUpdate command

---
+++ tests/Commands/AclFieldUpdateTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AdobeConnectClient\Tests\Commands;
+
+use AdobeConnectClient\Commands\AclFieldUpdate;
+use AdobeConnectClient\Parameter;
+
+class AclFieldUpdateTest extends TestCommandBase
+{
+    public function testExecute()
+    {
+        $extraParams = Parameter::instance()
+            ->set('extraField', 'extra value');
+
+        $command = new AclFieldUpdate(1, 'field', 'value', $extraParams);
+        $command->setClient($this->client);
+
+        $this->assertTrue($command->execute());
+    }
+
+    public function testException()
+    {
+        $this->expectException(\Exception::class);
+
+        $this->connection->overrideStatusWithNoAccess();
+
+        $command = new AclFieldUpdate(1, 'field', 'value');
+        $command->setClient($this->client);
+        $command->execute();
+    }
+}
