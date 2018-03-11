69d5925f178f56a7573ccb6054b45b3a1f40e54e
Bruno Gasparetto <brunogasparetto@gmail.com>
2018-03-11 17:41:49 -0400

    Delete duplicate responses

--- tests/Connection/File/routes.php
+++ tests/Connection/File/routes.php
@@ -7,11 +7,11 @@
  */
 return [
     'acl-field-update' => [
-        'ec1009edcfbc4d44063e3e4c7bf7f042bcbd2dd1' => 'acl-field-update-success',
-        'f9bfa92d710a9988edec5b75ae121de4003dedba' => 'acl-field-update-success',
-        '7fec835d624f38f4f6b1a4f93442ea8b5c4c4726' => 'acl-field-update-no-access',
-        '35f35a3fcea9c4b0f63e48b652e8ff41e175dc24' => 'acl-field-update-no-access',
-        'c0db054e609efa75105a7888ff851f177c2a7d06' => 'acl-field-update-invalid',
+        'ec1009edcfbc4d44063e3e4c7bf7f042bcbd2dd1' => 'status-ok',
+        'f9bfa92d710a9988edec5b75ae121de4003dedba' => 'status-ok',
+        '7fec835d624f38f4f6b1a4f93442ea8b5c4c4726' => 'status-no-access',
+        '35f35a3fcea9c4b0f63e48b652e8ff41e175dc24' => 'status-no-access',
+        'c0db054e609efa75105a7888ff851f177c2a7d06' => 'status-invalid',
     ],
 
     'common-info' => [
@@ -20,72 +20,72 @@ return [
     ],
 
     'group-membership-update' => [
-        '5e253185ad6e3a7e303d22f972a017ac5b02c799' => 'group-membership-update-success',
-        '6463d48e5069e62e884206d0bffdd96d905f42c6' => 'group-membership-no-access',
-        '12e9e36dbe3c9bfb3a37222f27019bae5efea4b1' => 'group-membership-update-invalid',
+        '5e253185ad6e3a7e303d22f972a017ac5b02c799' => 'status-ok',
+        '6463d48e5069e62e884206d0bffdd96d905f42c6' => 'status-no-access',
+        '12e9e36dbe3c9bfb3a37222f27019bae5efea4b1' => 'status-invalid',
     ],
 
     'list-recordings' => [
         '7c8db3e9804cd73bea5842d7507e229ad0b01ffb' => 'list-recordings-success',
-        '57b346ce02528b904643a12241f6488562d515f1' => 'list-recordings-no-data',
-        '6337d6a112fb8394ccf6126adf4c39f9c41dbee5' => 'list-recordings-no-access',
+        '57b346ce02528b904643a12241f6488562d515f1' => 'status-no-data',
+        '6337d6a112fb8394ccf6126adf4c39f9c41dbee5' => 'status-no-access',
     ],
 
     'login' => [
-        'd635c464148d1934d1195a30edba072b35e59e9c' => 'login-success',
-        '54d6988c059905b6c7a87bbc10c3e84f538115e3' => 'login-invalid',
+        'd635c464148d1934d1195a30edba072b35e59e9c' => 'status-ok',
+        '54d6988c059905b6c7a87bbc10c3e84f538115e3' => 'status-no-data',
     ],
 
     'logout' => [
-        'cac7b6bb995acffa265c6cc9c38c9904be3d4ad6' => 'logout',
+        'cac7b6bb995acffa265c6cc9c38c9904be3d4ad6' => 'status-ok',
     ],
 
     'meeting-feature-update' => [
-        '9d2cc3f416743c3a256d124643ab1d0c7f1ce65b' => 'meeting-feature-update-success',
-        'b49f99e9765f9554afbd1252075d5bd1653f1c00' => 'meeting-feature-update-no-access',
-        'b3a13d33abaa781720820d257e4e270dea3d996a' => 'meeting-feature-update-invalid',
+        '9d2cc3f416743c3a256d124643ab1d0c7f1ce65b' => 'status-ok',
+        'b49f99e9765f9554afbd1252075d5bd1653f1c00' => 'status-no-access',
+        'b3a13d33abaa781720820d257e4e270dea3d996a' => 'status-invalid',
     ],
 
     'permissions-info' => [
         '889f22d94f8698acf494e0b92a32af621b8d523a' => 'permissions-info-principal',
-        'cb92de166bc3ac9eb17061330521245f315789e6' => 'permissions-info-no-data',
-        'fe034c017cb5f28e6c5197cb4c60b2cb43f6e8b5' => 'permissions-info-no-access',
+        'cb92de166bc3ac9eb17061330521245f315789e6' => 'status-no-data',
+        'fe034c017cb5f28e6c5197cb4c60b2cb43f6e8b5' => 'status-no-access',
         '66199296f7cb96201b74d1b514d38ffd5d751da9' => 'permissions-info-all',
         '088ec38122a16dbbed309a960aeef2a67264f2c5' => 'permissions-info-filter-permission',
         '721100c4ce934c1cb3816d5806843832c1fb681f' => 'permissions-info-all',
         '0652074d72bec5542bc55ad052a27db8c1f41b75' => 'permissions-info-empty',
-        'b11a3d47c71ca83a8fff379b191f06351e659a70' => 'permissions-info-no-access',
+        'b11a3d47c71ca83a8fff379b191f06351e659a70' => 'status-no-access',
     ],
 
     'permissions-update' => [
-        '71e3d7c0567ec5429da14cd91b0f16a20d0a244d' => 'permission-update-success',
-        'eed55aa2f119637eb9f12efc120087d6b3eedbc5' => 'permission-update-success',
-        '403b845b83b942e00dc00d907829fa8c9f8bcd34' => 'permission-update-no-access',
-        'fdba3dd167f5071b7e28b6bf345ad17c836bf759' => 'permission-update-no-access',
+        '71e3d7c0567ec5429da14cd91b0f16a20d0a244d' => 'status-ok',
+        'eed55aa2f119637eb9f12efc120087d6b3eedbc5' => 'status-ok',
+        '403b845b83b942e00dc00d907829fa8c9f8bcd34' => 'status-no-access',
+        'fdba3dd167f5071b7e28b6bf345ad17c836bf759' => 'status-no-access',
     ],
 
     'principals-delete' => [
-        '6c39a7165c57b3339d1ddc39499f850cfa31bb99' => 'principals-delete-success',
-        '083cad6581fdbf158165192598040f2c18fcf9bd' => 'principals-delete-no-access',
+        '6c39a7165c57b3339d1ddc39499f850cfa31bb99' => 'status-ok',
+        '083cad6581fdbf158165192598040f2c18fcf9bd' => 'status-no-access',
     ],
 
     'principal-info' => [
         '49d78b23c62f9a7bc35704a4a5d4c6ab57cc3290' => 'principal-info-user',
         '618259e8dc03ad89e8261150b5adb14a9e460649' => 'principal-info-group',
-        '8d355bf7aea07113487665e0eedd26e04b252688' => 'principal-info-no-access',
+        '8d355bf7aea07113487665e0eedd26e04b252688' => 'status-no-access',
     ],
 
     'principal-update' => [
-        '0a3ff4f4bf08f7c18a08c4accb0c952f2663e2cf' => 'principal-update-no-access',
-        '7e8f86017c3168c211e0da2f739a6b29b4b0f988' => 'principal-update-no-access',
+        '0a3ff4f4bf08f7c18a08c4accb0c952f2663e2cf' => 'status-no-access',
+        '7e8f86017c3168c211e0da2f739a6b29b4b0f988' => 'status-no-access',
         'd707626582cc0bea8be5a10011b4fb6afcb62afa' => 'principal-update-create-user',
         '1177141f4482789d55daffa0cda76b2a327ba6fd' => 'principal-update-create-group',
-        'd74fb1b82f258a0066340ef1ca4172bbdfb3a60f' => 'principal-update-update',
-        '4ead5c80628ae73a4343a588979a2f7fef1e7798' => 'principal-update-update',
+        'd74fb1b82f258a0066340ef1ca4172bbdfb3a60f' => 'status-ok',
+        '4ead5c80628ae73a4343a588979a2f7fef1e7798' => 'status-ok',
     ],
 
     'principal-list' => [
-        'c228737ee01c96f5b6c3c88ab94cb390347e3647' => 'principal-list-no-access',
+        'c228737ee01c96f5b6c3c88ab94cb390347e3647' => 'status-no-access',
         'c7d426c3410ec12370e5a4319f8aeff934c9565f' => 'principal-list-all',
         '0f9879a925d933ca16dd095ba2fa5944ff13de0f' => 'principal-list-sorter',
         'ec5d33500aa52c47e176076b19037113a94db903' => 'principal-list-filter',
@@ -96,9 +96,9 @@ return [
     ],
 
     'user-update-pwd' => [
-        'dba78015dd3532813b513ac5f4d2825fbb712f8e' => 'user-update-pwd-no-access',
-        'ff9d991b034d2bbc4fe76e71e5b64ba1cac353ed' => 'user-update-pwd-success',
-        '6e19a6356eb0449548c24b0182cbc5efd0d60fb3' => 'user-update-pwd-success',
+        'dba78015dd3532813b513ac5f4d2825fbb712f8e' => 'status-no-access',
+        'ff9d991b034d2bbc4fe76e71e5b64ba1cac353ed' => 'status-ok',
+        '6e19a6356eb0449548c24b0182cbc5efd0d60fb3' => 'status-ok',
     ],
 
     '' => [
