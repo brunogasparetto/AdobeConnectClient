00be00d6e5cebb56a62014c86e98447e82afa157
Bruno Gasparetto <brunogasparetto@gmail.com>
2018-03-11 18:12:18 -0400

    Create tests for ScoMove command

---
+++ tests/Commands/ScoMoveTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace AdobeConnectClient\Tests\Commands;
+
+use AdobeConnectClient\Commands\ScoMove;
+use AdobeConnectClient\Exceptions\InvalidException;
+use AdobeConnectClient\Exceptions\NoAccessException;
+
+class ScoMoveTest extends TestCommandBase
+{
+    public function testNoAccess()
+    {
+        $this->userLogout();
+
+        $command = new ScoMove(1, 1);
+        $command->setClient($this->client);
+
+        $this->expectException(NoAccessException::class);
+
+        $command->execute();
+    }
+
+    public function testInvalid()
+    {
+        $this->userLogin();
+
+        $command = new ScoMove(1, 2);
+        $command->setClient($this->client);
+
+        $this->expectException(InvalidException::class);
+
+        $command->execute();
+    }
+
+    public function testMove()
+    {
+        $this->userLogin();
+
+        $command = new ScoMove(1, 1);
+        $command->setClient($this->client);
+
+        $this->assertTrue($command->execute());
+    }
+}
